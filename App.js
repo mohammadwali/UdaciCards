54c406a95a9355ba5bd245b45b70101e865c798b
Mohammad Wali <mohdwali@live.com>
2017-10-14 16:43:51 +0530

    Setting up redux

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import {View} from 'react-native'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
 import {orange} from './utils/colors'
+
+import reducer from './reducers/index'
+
 import StatusBar from './components/StatusBar'
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={{flex: 1}}>
-                <StatusBar backgroundColor={orange}/>
-            </View>
+            <Provider store={createStore(reducer)}>
+                <View style={{flex: 1}}>
+                    <StatusBar backgroundColor={orange}/>
+                </View>
+            </Provider>
         );
     }
 }
