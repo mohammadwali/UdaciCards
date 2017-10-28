44908d0f0bfdd39df32004d2a471153d1306bd7c
Mohammad Wali <mohdwali@live.com>
2017-10-28 16:59:15 +0530

    Adding Quiz Result Titles

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,16 @@
+const QuizTitles = {
+    1: ["Keep trying!", "Better luck next time!", "Try once more.."],
+    2: ["Nice job!", "Sweet!", "Cool!", "Looks good!"],
+    3: ["Great!", "Awesome!", "Super!", "Perfect!"]
+}
+
+
 export function formatCardsCount(availableCards) {
     return availableCards > 0 ? (availableCards + (availableCards > 1 ? ' cards' : ' card')) : 'No cards';
 }
 
-export function getQuizResultTitle(score) {
-    return "Awesome!"
+export function getQuizResultTitle(correctAnswers, totalQuestions) {
+    const percent = Math.round((correctAnswers / totalQuestions) * 100)
+    const category = (percent <= 33) ? 1 : (percent <= 66) ? 2 : 3
+    return QuizTitles[category][Math.floor(Math.random() * QuizTitles[category].length)]
 }
